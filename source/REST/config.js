21609fdafa507627667d4b2288e14f4c6f01693a
Evgen Osiptsov <evgen.osiptsov@gmail.com>
2018-07-29 16:39:06 +0300

    styles

--- source/REST/config.js
+++ source/REST/config.js
@@ -6,7 +6,7 @@ const TOKEN = 'rtASDLastuev77';
 
 const SOCKET_URL = 'https://lab.lectrum.io';
 
-const MAIN_URL = getFullApiUrl("https://lab.lectrum.io/redux/api/feed/", GROUP_ID);
+const MAIN_URL = "https://lab.lectrum.io/redux/api/feed/";
 const SIGNUP_URL = getFullApiUrl("https://lab.lectrum.io/redux/api/user/", GROUP_ID)
 const SIGNIN_URL = "https://lab.lectrum.io/redux/api/user/login"
 
